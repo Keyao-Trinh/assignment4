9721b548c0464697035ba62a5ceaf97582c3b175
Keyao-Trinh <trinhkeyao@gmail.com>
2026-04-30 12:13:32 -0400

    Update ReviewsView.tsx

--- assignmentfor/src/views/movies/ReviewsView.tsx
+++ assignmentfor/src/views/movies/ReviewsView.tsx
@@ -12,15 +12,18 @@ export const ReviewsView = () => {
   }
 
   return (
-    <section className="min-h-screen bg-gray-900 text-white space-y-6">
+    <section className="space-y-5 p-5">
       <h2 className="text-2xl font-bold">Reviews</h2>
-      {!data.results.length && <p className="text-gray-400 text-center">No reviews available.</p>}
-      {data.results.slice(0, 5).map((review) => (
-        <div key={review.id} className="bg-gray-800 p-5 rounded-xl shadow">
-          <p className="text-sm text-gray-400 mb-2">By {review.author}</p>
-          <p className="text-gray-300 text-sm leading-relaxed line-clamp-6">{review.content}</p>
-        </div>
-      ))}
+      {data.results.length ? (
+        data.results.slice(0, 5).map((review) => (
+          <div key={review.id} className="bg-gray-800 p-5 rounded-xl shadow">
+            <p className="text-sm text-gray-400 mb-2">By {review.author}</p>
+            <p className="text-gray-300 text-sm leading-relaxed line-clamp-6">{review.content}</p>
+          </div>
+        ))
+      ) : (
+        <p className="text-gray-400 text-center">No reviews available.</p>
+      )}
     </section>
   );
 };
