0260de5b24c5d4b25d52694fd56ed812d2f4d39f
Keyao-Trinh <trinhkeyao@gmail.com>
2026-04-30 22:36:16 -0400

    heh, I now know why there was no css/tailwind

--- assignmentfor/src/main.tsx
+++ assignmentfor/src/main.tsx
@@ -1,4 +1,5 @@
 import { App } from './App.tsx';
+import '@/index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
