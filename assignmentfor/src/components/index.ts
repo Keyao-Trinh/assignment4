70cf63e0614c1c5f1db6239672a3d0d77b4d9599
Keyao-Trinh <trinhkeyao@gmail.com>
2026-05-04 12:22:42 -0400

    Update index.ts, now Modal and footer are there

--- assignmentfor/src/components/index.ts
+++ assignmentfor/src/components/index.ts
@@ -6,3 +6,5 @@ export * from '@/components/Link';
 export * from '@/components/LinkGroup';
 export * from '@/components/Pagination';
 export * from '@/components/SearchBar';
+export * from '@/components/Modal';
+export * from '@/components/Footer';
