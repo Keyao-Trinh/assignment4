06eb97f6bbcc65fefd7a85c7bf70ae4e6f048fff
Keyao-Trinh <trinhkeyao@gmail.com>
2026-05-04 12:19:18 -0400

    Made footer

--- assignmentfor/src/components/Footer.tsx
+++ assignmentfor/src/components/Footer.tsx
@@ -1,17 +1,8 @@
-// // import { Link } from '@/components';
-// // import { SearchView } from '@/views/SearchView';
-
-// export const Footer = () => {
-//   return (
-//     <header>
-//       <nav className="flex gap-4 p-4 bg-gray-800">
-//         <h1 className="text-l font-bold text-white-900"></h1>
-//         <link to="https://github.com/Keyao-Trinh/assignment4">GitHub</link>
-//         {/* <Link to="/tv/catagory/airing_today">TV</Link> */}
-//         {/* <Link to="/trending/movies">Trending</Link> */}
-//         {/* <Link to="/genre/movies/adventure">Genre</Link> */}
-//         {/* <Link to="/search">Search</Link> */}
-//       </nav>
-//     </header>
-//   );
-// };
+export const Footer = () => {
+  return (
+    <footer className="flex gap-4 p-4 bg-gray-800">
+        <h1 className="text-l font-bold text-white-500">Footer conponent</h1>
+        <a className="text-1 font-bold text-white-500" href="https://github.com/Keyao-Trinh/assignment4">Github</a>
+    </footer>
+  );
+};
