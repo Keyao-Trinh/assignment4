46b3f9db5efeec38591fe91a6d41a7324d51d83d
Keyao-Trinh <trinhkeyao@gmail.com>
2026-05-04 18:28:56 -0400

    Update TrailerView.tsx (but I really just swiched back to a different older/new one because this one wasn't working before :|)

--- assignmentfor/src/views/movies/TrailerView.tsx
+++ assignmentfor/src/views/movies/TrailerView.tsx
@@ -1,15 +1,13 @@
 import { MOVIE_ENDPOINT } from '@/core/constants';
-import type { DetailRepsonse } from '@/core/types';
+import type { MovieRespsonse } from '@/core/types';
 import { useTmdb } from '@/hooks';
 import { useParams } from 'react-router-dom';
-// popular, Now Playing, Upcoming, and Top
 
 export const TrailerView = () => {
-//   const navigate = useNavigate();
   const { id } = useParams();
-  const { data } = useTmdb<DetailRepsonse>(`${MOVIE_ENDPOINT}/${id}`, { append_to_response: 'videos' }, []);
+  const { data } = useTmdb<MovieRespsonse>(`${MOVIE_ENDPOINT}/${id}/videos`, { append_to_response: 'videos' }, []);
 
-  const trailerVideo =
+ const trailerVideo =
     data?.videos?.results.find(
       (video) => video.site === 'YouTube' && video.type === 'Trailer' && video.name?.toLowerCase().includes('official')
     ) || data?.videos?.results.find((video) => video.site === 'YouTube' && video.type === 'Trailer');
@@ -19,20 +17,19 @@ export const TrailerView = () => {
   }
 
   return (
-    <section className="max-w-[1200px] mx-auto p-10">
-      <h2 className="text-2xl font-bold mb-6">Credits</h2>
-      <div className="space-y-4">
-          {trailerVideo && (
-            <div className="aspect-video">
-              <iframe
-                className="w-full h-full rounded-xl"
-                src={`https://www.youtube.com/embed/${trailerVideo.key}`}
-                title="Movie Trailer"
-                allowFullScreen
-              />
-            </div>
-          )}
-      </div>
+    <section className="min-h-screen bg-gray-900 text-white">
+      <h2 className="text-2xl font-bold mb-6">Trailers</h2>
+      {!data.cast.length && <p className="text-gray-400 text-center">No trailer available.</p>}
+      {trailerVideo && (
+              <div className="aspect-video w-[50%]">
+                <iframe
+                  className="h-full w-full rounded-xl"
+                  src={`https://www.youtube.com/embed/${trailerVideo.key}`}
+                  title={trailerVideo.name}
+                  allowFullScreen
+                />
+              </div>
+            )}
     </section>
   );
 };
